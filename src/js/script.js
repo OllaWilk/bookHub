97984b29c9bd180d60470ac7ad801aa0057e8a8e
OllaWilk <aleksandra.wilk.berserksoft@gmail.com>
2023-10-10 16:39:21 +0200

    add mockup of general page

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
-console.log('works');
+const hamburgerWrap = document.querySelector('.hamburger-wrap ');
+const sideNavigation = document.querySelector('#side-navigation');
+const hamburgerIcon = document.querySelectorAll('.hamburger-wrap i');
+
+hamburgerWrap.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  hamburgerIcon.forEach((icon) => icon.classList.toggle('close'));
+
+  sideNavigation.classList.toggle('toggle');
+});
